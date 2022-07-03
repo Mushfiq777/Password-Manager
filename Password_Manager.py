85e4cc31b26842b098ed4aceab4e790a50cdc198
Mushfiq777 <mushfiq7771@gmail.com>
2022-07-03 16:47:22 +0300

    added delete function- delete function complete

--- Password_Manager.py
+++ Password_Manager.py
@@ -67,7 +67,7 @@ def search():
                 count+=1
 
         if count != 0:
-            print("Account(s) found!".center(width))
+            print("Account found!".center(width))
         else:
             print("No account found".center(width))
     with open("key.txt","rb") as e:
@@ -110,7 +110,7 @@ def edit():
             acc[2] = fer.decrypt(acc[2].encode())
             acc[2] = acc[2].decode()
             if acc_edit==acc[0]:
-                print("Account found!")
+                print("Account found!".center(width))
                 found = True
                
                 print(b1.center(width))
@@ -130,7 +130,6 @@ def edit():
                     elif uorp == "p":
                         new_password = input("Enter new password: ".center(width).rstrip())
                         acc[2] = new_password
-                        # acc[1] = username
                         break
                     else:
                         print("Invalid input Please try again".center(width))
@@ -153,7 +152,72 @@ def edit():
         print("No account found by the name.".center(width))
     with open("passwords.txt","w") as p:
         for i in data:
-            p.write(i+"\n")               
+            p.write(i+"\n") 
+            
+def delete_all():
+    open("passwords.txt","w").close()
+    print("All records deleted successfully".center(width))
+
+def delete_s():
+    with open("key.txt","rb") as e:
+        key = e.read()
+        fer = Fernet(key)
+    acc_d = input("Enter the account name you want to delete: ".center(width).rstrip())
+    found = False
+    newdata = []
+    with open("passwords.txt","r") as d:
+        for line in d.readlines():
+            linestripped = line.strip()
+            l = linestripped.split("|")
+            pwd = fer.decrypt(l[2].encode())
+            pwd = pwd.decode()
+            username = fer.decrypt(l[1].encode())
+            username = username.decode()
+            adj = 40
+            
+            if acc_d == l[0]:
+                found = True
+                print("Account found!".center(width))
+                print(b1.center(width))
+                print("Account:".center(width-adj).rstrip(),acc_d.center(width).lstrip())
+                print("Username:".center(width-adj).rstrip(),username.center(width).lstrip())
+                print("Password:".center(width-adj).rstrip(),pwd.center(width).lstrip())
+                print(b1.center(width))
+                while True:
+                    
+                    input1 = input("Are you sure you want to delete?(y/n): ".center(width).rstrip()).lower()
+                    if input1=="y":
+                        print("Account deleted successfully!".center(width))
+                        break
+                    elif input1=="n":
+                        newdata.append(linestripped)
+                        print("Account not deleted".center(width).rstrip())
+                        break
+                    else:
+                        print("Invalid Input Please Try Again".center(width))  
+            else:
+                newdata.append(linestripped)
+    if found==False:
+        print("No account found by the name.".center(width))
+    with open("passwords.txt","w") as p:
+        for i in newdata:
+            p.write(i+"\n")
+    
+            
+            
+def delete():
+    while True:
+        
+        input1 = input("Do you want to delete all records or a specifc one?(a/s): ".center(width).rstrip()).lower()
+        if input1=="a":
+            delete_all()
+            break
+        elif input1=="s":
+            delete_s()
+            break
+        else:
+            print("Invalid Input Please Try Again".center(width).rstrip())
+                  
                               
 logo = (
 "██████╗░░█████╗░░██████╗░██████╗░██╗░░░░░░░██╗░█████╗░██████╗░██████╗░\n"+
@@ -189,9 +253,9 @@ while True:
     else:
         print("Incorrect master password Try again!".center(width))
     
-options = ["v","a","s","q","e"]
+options = ["v","a","s","q","e","d"]
 while True:
-    ask = input("Do you want to view, add, edit or search password(v/a/e/s) or quit(q): ".center(width).rstrip()).lower()
+    ask = input("Do you want to view, add, edit, delete or search password(v/a/e/d/s) or quit(q): ".center(width).rstrip()).lower()
     if ask in options:
         if ask == "v":
             view()
@@ -201,11 +265,11 @@ while True:
             search()
         elif ask=="e":
             edit()
+        elif ask=="d":
+            delete()    
         else:
             print("You have exited the password manager..".center(width))
             break
     else:
         print("Invalid input please try again".center(width).rstrip())    
         continue
-    
-
